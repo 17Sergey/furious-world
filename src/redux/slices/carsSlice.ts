79a8e83ac6a5a906c6a9095f26f20bbc8fc17f4a
Sergey <serzh.kachalyuk@gmail.com>
2023-12-10 19:03:44 +0300

    photo add

--- src/redux/slices/carsSlice.ts
+++ src/redux/slices/carsSlice.ts
@@ -33,60 +33,212 @@ const characters = [
                 id: 0,
                 carName: "1970 Dodge Charger R/T",
                 carImages: [
-                    "https://www.legendlines.com/cdn/shop/articles/Charger_Fast_Furious_00_800x.jpg?v=1616602301",
-                    "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/02/Fast-and-Furious-Dom-Dodge-Charger-RT.jpg?q=50&fit=crop&w=1500&dpr=1.5",
-                    "https://www.motorious.com/content/images/2021/04/All-Electric-Dodge-Charger-Stars-In-Next-Fast---Furious--3.jpg",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_1.webp",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
                 ],
             },
             {
                 id: 1,
                 carName: "1970 Chevrolet Chevelle SS",
                 carImages: [
-                    "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/02/Fast-and-Furious-Dom-Chevrolet-Chevelle-SS.jpg?q=50&fit=crop&w=1500&dpr=1.5",
-                    "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/02/Fast-and-Furious-Dom-Dodge-Charger-RT.jpg?q=50&fit=crop&w=1500&dpr=1.5",
-                    "https://www.motorious.com/content/images/2021/04/All-Electric-Dodge-Charger-Stars-In-Next-Fast---Furious--3.jpg",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Chevrolet%20Chevelle/Chevrolet%20Chevelle1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
                 ],
             },
             {
                 id: 2,
                 carName: "2009 Dodge Challenger SRT-8",
                 carImages: [
-                    "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/02/Fast-and-Furious-Dom-Dodge-Challenger-SRT8.jpg?q=50&fit=crop&w=1500&dpr=1.5",
-                    "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/02/Fast-and-Furious-Dom-Dodge-Charger-RT.jpg?q=50&fit=crop&w=1500&dpr=1.5",
-                    "https://www.motorious.com/content/images/2021/04/All-Electric-Dodge-Charger-Stars-In-Next-Fast---Furious--3.jpg",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Challenger%20SRT-8/DodgeChallenger1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
                 ],
             },
             {
                 id: 3,
                 carName: "2021 Dodge Charger SRT Hellcat Redeye Widebody",
                 carImages: [
-                    "https://i0.wp.com/moparinsiders.com/wp-content/uploads/2023/02/Fast-X-Trailer-Screenshots.-Universal-Studios.-5.jpeg?resize=708%2C398&ssl=1",
-                    "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/02/Fast-and-Furious-Dom-Dodge-Charger-RT.jpg?q=50&fit=crop&w=1500&dpr=1.5",
-                    "https://www.motorious.com/content/images/2021/04/All-Electric-Dodge-Charger-Stars-In-Next-Fast---Furious--3.jpg",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat1.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat2.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
                 ],
             },
         ]
     },
-    // {
-    //     id: 1,
-    //     name: "Brian",
-    // },
-    // {
-    //     id: 2,
-    //     name: "Mia",
-    // },
-    // {
-    //     id: 3,
-    //     name: "Ramsey",
-    // },
-    // {
-    //     id: 4,
-    //     name: "Tej",
-    // },
+    {
+        id: 1,
+        name: "Brian",
+        cars: [
+            {
+                id: 0,
+                carName: "1970 Dodge Charger R/T",
+                carImages: [
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_1.webp",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 1,
+                carName: "1970 Chevrolet Chevelle SS",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Chevrolet%20Chevelle/Chevrolet%20Chevelle1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 2,
+                carName: "2009 Dodge Challenger SRT-8",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Challenger%20SRT-8/DodgeChallenger1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 3,
+                carName: "2021 Dodge Charger SRT Hellcat Redeye Widebody",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat1.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat2.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+        ]
+    },
+    {
+        id: 2,
+        name: "Mia",
+        cars: [
+            {
+                id: 0,
+                carName: "1970 Dodge Charger R/T",
+                carImages: [
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_1.webp",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 1,
+                carName: "1970 Chevrolet Chevelle SS",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Chevrolet%20Chevelle/Chevrolet%20Chevelle1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 2,
+                carName: "2009 Dodge Challenger SRT-8",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Challenger%20SRT-8/DodgeChallenger1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 3,
+                carName: "2021 Dodge Charger SRT Hellcat Redeye Widebody",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat1.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat2.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+        ]
+    },
+    {
+        id: 3,
+        name: "Ramsey",
+        cars: [
+            {
+                id: 0,
+                carName: "1970 Dodge Charger R/T",
+                carImages: [
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_1.webp",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 1,
+                carName: "1970 Chevrolet Chevelle SS",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Chevrolet%20Chevelle/Chevrolet%20Chevelle1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 2,
+                carName: "2009 Dodge Challenger SRT-8",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Challenger%20SRT-8/DodgeChallenger1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 3,
+                carName: "2021 Dodge Charger SRT Hellcat Redeye Widebody",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat1.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat2.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+        ]
+    },
+    {
+        id: 4,
+        name: "Tej",
+        cars: [
+            {
+                id: 0,
+                carName: "1970 Dodge Charger R/T",
+                carImages: [
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_1.webp",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 1,
+                carName: "1970 Chevrolet Chevelle SS",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Chevrolet%20Chevelle/Chevrolet%20Chevelle1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 2,
+                carName: "2009 Dodge Challenger SRT-8",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Challenger%20SRT-8/DodgeChallenger1.jpg?raw=true",
+                    "https://raw.githubusercontent.com/17Sergey/furious-world/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_2.webp",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+            {
+                id: 3,
+                carName: "2021 Dodge Charger SRT Hellcat Redeye Widebody",
+                carImages: [
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat1.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20SRT%20Hellcat/Dodge%20Charger%20SRT%20Hellcat2.jpg?raw=true",
+                    "https://github.com/17Sergey/furious-world/blob/main/src/assets/Dominic/Cars/Dodge%20Charger%20RT-8/Dodge%20Charger%20RT-8_3.jpg?raw=true",
+                ],
+            },
+        ]
+    },
 ]
 
 const customInitialState: CharacterCarsState = {
-    activeCar: {...characters[0].cars[0], activeImage: characters[0].cars[0].carImages[0]},
+    activeCar: { ...characters[0].cars[0], activeImage: characters[0].cars[0].carImages[0] },
     characters
 }
 
@@ -96,7 +248,7 @@ const carsSlice = createSlice({
     initialState: customInitialState,
     reducers: {
         changeActiveCar: (state: CharacterCarsState, action: PayloadAction<CharacterCarType>) => {
-            state.activeCar = {...action.payload, activeImage: action.payload.carImages[0]};
+            state.activeCar = { ...action.payload, activeImage: action.payload.carImages[0] };
         },
         changeActiveCarImage: (state: CharacterCarsState, action: PayloadAction<string>) => {
             state.activeCar.activeImage = action.payload;
